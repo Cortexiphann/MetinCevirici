256b7504b20821efd3c17e356261f0c726b25943
Furkan Filikci <87387312+Cortexiphann@users.noreply.github.com>
2023-07-19 22:54:01 +0300

    metincevirici.py

--- metincevirici.py
+++ metincevirici.py
@@ -11,12 +11,24 @@ class MetinDonusturucu:
     
     def kucuk_harfe_cevir(self):
         return self.metin.lower()
-    
-# Kullanıcıdan bir metin iste
-metin = input("Dönüştürmek istediğiniz metni girin: ")
 
+# Kullanıcıdan bir metin iste
+metin = input("Dönüştürmek İstediğiniz Metni Girin: ")
 metin_donusturucu = MetinDonusturucu(metin)
 
-print("Metnin Ters Çevrilmiş Hali:", metin_donusturucu.ters_cevir())
-print("Metnin Büyük Harflerle Yazılmış Hali:", metin_donusturucu.buyuk_harfe_cevir())
-print("Metnin Küçük Harflerle Yazılmış Hali:", metin_donusturucu.kucuk_harfe_cevir())
+# Kullanıcıya dönüştürme seçeneklerini göster
+print("Dönüştürme Seçenekleri:")
+print("1. Metnin Ters Çevrilmiş Hali")
+print("2. Metnin Büyük Harflerle Yazılmış Hali")
+print("3. Metnin Küçük Harflerle Yazılmış Hali")
+
+secim = input("Yapmak istediğiniz dönüşümün numarasını girin (1/2/3): ")
+
+if secim == "1":
+    print("Metnin Ters Çevrilmiş Hali:", metin_donusturucu.ters_cevir())
+elif secim == "2":
+    print("Metnin Büyük Harflerle Yazılmış Hali:", metin_donusturucu.buyuk_harfe_cevir())
+elif secim == "3":
+    print("Metnin Küçük Harflerle Yazılmış Hali:", metin_donusturucu.kucuk_harfe_cevir())
+else:
+    print("Geçersiz seçim! Lütfen 1, 2 veya 3 numaralarından birini girin.")
